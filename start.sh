fb22544c6ee66486e8b77cbec7971bae047c8420
duartebranco <duartebranco35@gmail.com>
2026-04-30 19:17:17 +0100

    fix model download path

--- start.sh
+++ start.sh
@@ -4,9 +4,8 @@ python -c "
 from huggingface_hub import snapshot_download
 snapshot_download(
     'duartebranco/cardiovascular-disease-models',
-    local_dir='/app/models',
+    local_dir='/app',
     repo_type='model',
-    local_dir_use_symlinks=False,
 )
 print('Models ready')
 "
